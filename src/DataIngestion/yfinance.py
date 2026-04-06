21199747ccde4d25639cb4aa285e1fd604670e14
jazzy-coding <jasperwilson99@gmail.com>
2026-04-06 13:00:28 +0100

    get data from yfinance api

--- src/DataIngestion/yfinance.py
+++ src/DataIngestion/yfinance.py
@@ -1,11 +1,13 @@
 """Module for yfinance market data ingestion."""
+
 import datetime as dt
-import polars as pl
+import pandas as pd
 import yfinance as yf
 
-def get_market_data(tickers: list[str], start_date: dt.date, end_date: dt.date) -> pl.DataFrame:
+
+def get_market_data(
+    tickers: list[str], start_date: dt.date, end_date: dt.date
+) -> pd.DataFrame | None:
     """Function to get market data from the yfinance API."""
-    return pl.DataFrame()
-
-
-
+    df = yf.download(tickers=tickers, start=start_date, end=end_date)
+    return df
