65d28ac072c223510799517439c0c4f866390063
jazzy-coding <jasperwilson99@gmail.com>
2026-04-06 17:33:57 +0100

    Refine unit test logic

--- tests/DataIngestion/test_yfinance.py
+++ tests/DataIngestion/test_yfinance.py
@@ -44,11 +44,25 @@ def test_get_market_data_no_tickers():
     tickers = []
     start_date = dt.date.today() - dt.timedelta(days=365)
     end_date = dt.date.today()
-    mock_df = pd.DataFrame()
 
     # act and assert
     with patch(
         "src.DataIngestion.yfinance.yf.download", side_effect=ValueError
-    ) as mock_download:
-        with pytest.raises(ValueError):
+    ):
+        with pytest.raises(ValueError, match="Tickers must not be empty."):
+            get_market_data(tickers=tickers, start_date=start_date, end_date=end_date)
+
+
+def test_get_market_data_invalid_dates():
+    """Test 'get_market_data' function with invalid dates."""
+    # arrange
+    tickers = ['VUSA.L', 'VUKG.L']
+    start_date = dt.date.today()
+    end_date = dt.date.today() - dt.timedelta(days=365)
+
+    # act and assert
+    with patch(
+        "src.DataIngestion.yfinance.yf.download", side_effect=ValueError
+    ):
+        with pytest.raises(ValueError, match="Start date must be before end date."):
             get_market_data(tickers=tickers, start_date=start_date, end_date=end_date)
